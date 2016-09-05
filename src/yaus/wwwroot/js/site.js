f5d423429374c1b6cf0b024b25de84c09fbce9ce
Michael Levy <michael.levy@sds-consulting.com>
2016-09-05 18:00:42 -0400

    started UI work

--- src/yaus/wwwroot/js/site.js
+++ src/yaus/wwwroot/js/site.js
@@ -1,1 +1,20 @@
 ﻿// Write your Javascript code.
+
+$(function() {
+    var $fullUrl = $("#fullUrl");
+    var $shortenButton = $("#shortenbutton");
+    var $shortenUrl = $("#shortenedUrl");
+    var $copyButton = $("#copyButton");
+
+    $shortenButton.on("click", () => {
+       
+    });
+
+    $fullUrl.on("keyup", () => {
+        $shortenButton.prop("disabled", $fullUrl.val() === "");
+    });
+
+    $shortenUrl.on("change", () => {
+        $copyButton.prop("disabled", $shortenUrl.val() === "");
+    });
+});
